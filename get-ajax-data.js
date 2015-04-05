c3edf1bedf97bdc20eb6251fed2e934c0785fa0d
Alicia Guerra <aiguerra@ucsd.edu>
2015-04-05 11:54:51 -0700

    Update get-ajax-data.js

--- get-ajax-data.js
+++ get-ajax-data.js
@@ -8,4 +8,14 @@ x.hr.open('get', 'send-ajax-data.php');
 xhr.onreadystatechange=function(){
 var DONE=4; //ReadyState4 means request is done.
 var OK=200;//status 200 is a successful return.
+if(xhr.readyState == DONE){
+  alert(xhr.responseText); //this is the returned text
+}
+else{
+  alert('Error:' +xhr.status); //An error occurred during the request.
+}
+}
+};
+//send the request to ajax-data.php
+xhr.send(null);
 
